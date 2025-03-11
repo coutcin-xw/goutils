9e01662950bd6b74346d20d8380ba2d6f012bf66
CoutCin <1473899495@qq.com>
2025-03-11 15:47:28 +0800

    v0.1.7

--- nettools/nettools.go
+++ nettools/nettools.go
@@ -235,9 +235,9 @@ func IsIPInCIDR(ip string, cidr string) bool {
 }
 
 type InterfaceIpInfo struct {
-	InsecureSkipVerifyfaceName string
-	IfaceIsUp                  bool
-	IfaceIpNets                []net.IPNet
+	IfaceName   string
+	IfaceIsUp   bool
+	IfaceIpNets []net.IPNet
 }
 
 func GetIpv6Global() ([]InterfaceIpInfo, error) {
